0b05f275e3b2d15bfe14b370d78bb246b6515568
hari <le31052003@gmail.com>
2024-11-12 22:31:11 +0700

    add model

---
+++ back-end/src/controllers/BookingController.js
@@ -0,0 +1,53 @@
+const Booking = require('../models/Booking');
+const { multipleMongooseToObject } = require('../../util/mongoose');
+const { mongooseToObject } = require('../../util/mongoose');
+
+class BookingController {
+    //[POST] /booking
+    async show(req, res) {
+        //verify
+        Booking.create({
+            place,
+            checkIn,
+            checkOut,
+            numberOfGuests,
+            phone,
+            price,
+            user: userData.id,
+        })
+        try{
+            res.json(doc);
+        }
+        catch{
+            throw err;
+        }
+    }
+
+    //[Get] /booking
+    async getBookings(req, res) {
+            res.json(
+                await Booking.find({ user: userData.id }).populate('place'),
+            );
+    }
+
+    //[DELETE] /booking/:id
+    async deleteID(req, res) {
+        const { id } = req.params;
+        res.json(await Booking.deleteOne({ user: userData.id, _id: id }));
+    }
+
+    async bookingManager(req, res) {
+        const ownerId = userData.id;
+        const places = await Place.find({ owner: ownerId });
+        const placeIds = places.map(place => place._id);
+        res.json(await Booking.find({
+            place: {
+                $in: placeIds
+            }
+        }).populate('place').populate('user'))
+    }
+
+    
+}
+
+module.exports = new BookingController();
