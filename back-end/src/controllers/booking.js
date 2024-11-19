48658088d30ce7b7c892ea975c55ee7ee0aa0803
hari <le31052003@gmail.com>
2024-11-19 23:27:33 +0700

    controller without jwt

--- back-end/src/controllers/booking.js
+++ back-end/src/controllers/booking.js
@@ -3,35 +3,44 @@ const Booking = require('../models/booking');
 class BookingController {
     //[POST] /booking
     async show(req, res) {
-        //verify
-        Booking.create({
-            place,
-            checkIn,
-            checkOut,
-            numberOfGuests,
-            phone,
-            price,
-            user: userData.id,
-        })
-        try{
-            res.json(doc);
-        }
-        catch{
-            throw err;
+        try {
+            const {id, place, checkIn, checkOut, numberOfGuests, phone, price, name } = req.body;
+            const booking = await Booking.create({
+                place,
+                checkIn,
+                checkOut,
+                numberOfGuests,
+                phone,
+                price,
+                name, 
+                user: id, 
+            });
+
+            res.json(booking);
+        } catch (err) {
+            res.status(500).json({ message: 'Có lỗi xảy ra khi tạo booking', error: err.message });
         }
     }
 
+
     //[Get] /booking
     async getBookings(req, res) {
-            res.json(
-                await Booking.find({ user: userData.id }).populate('place'),
-            );
+        const { id } = req.params;  
+        try {
+            const bookings = await Booking.find({ user: id }).populate('place');
+            if (bookings.length === 0) {
+                return res.status(404).json({ message: 'Không tìm thấy booking cho người dùng này.' });
+            }
+            res.json(bookings);
+        } catch (error) {
+            res.status(500).json({ message: 'Lỗi khi truy xuất bookings', error });
+        }
     }
 
     //[DELETE] /booking/:id
     async deleteID(req, res) {
         const { id } = req.params;
-        res.json(await Booking.deleteOne({ user: userData.id, _id: id }));
+        res.json(await Booking.deleteOne({ _id: id }));
     }
 
     async bookingManager(req, res) {
