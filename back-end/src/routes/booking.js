eb7ea76e38facfba4f825df2a508304d39669706
hari <le31052003@gmail.com>
2024-11-19 23:32:51 +0700

    add params in get url (without jwt)

--- back-end/src/routes/booking.js
+++ back-end/src/routes/booking.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bookingController = require('../controllers/booking');
 
 router.delete('/:id', bookingController.deleteID);
-router.get('/', bookingController.getBookings);
+router.get('/:id', bookingController.getBookings);
 router.post('/', bookingController.show);
 
 module.exports = router;
