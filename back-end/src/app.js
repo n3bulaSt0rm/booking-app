81217c84a16317841f8c77dccaf80c95d2989d3f
hari <le31052003@gmail.com>
2024-11-19 23:26:49 +0700

    add require dotenv

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express');
 const connectDatabase = require('./config/database');
 const cors = require('cors');
@@ -19,5 +20,8 @@ app.use(
 );
 
 initRoutes(app);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 module.exports = app;
