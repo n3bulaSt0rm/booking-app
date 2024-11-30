32362f1e8107e111b8c13edc37e61171afd892b4
n3bulaSt0rm <hainguyen.4work@gmail.com>
2024-11-30 18:36:22 +0700

    refactor v2

---
+++ frontend-v2/src/Footer.jsx
@@ -0,0 +1,21 @@
+export default function Footer() {
+  return (
+    <div className="mt-auto pt-10 px-14 pb-6 bg-gray-100 flex justify-between">
+      <div>
+        <h1 className="font-bold text-xl pb-2">GoBooking</h1>
+        <h1 className="text-gray-500">
+          Đem đến trải nghiệm tuyệt vời nhất <br />
+          cho bạn!
+        </h1>
+      </div>
+      <div className="text-gray-500 text-right">
+        <h1 className="font-semibold text-black">Thông tin liên hệ</h1>
+        <h1>Nhóm 21</h1>
+        <h1>
+          Công nghê Web và<br />
+          Dịch vụ trực tuyến
+        </h1>
+      </div>
+    </div>
+  );
+}
