8bb795da4efb39431fa7cb1cec518fe6e12902bc
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2024-12-03 21:09:36 +0700

    update

--- frontend-v2/src/UserContext.jsx
+++ frontend-v2/src/UserContext.jsx
@@ -1,22 +1,39 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+// import React from 'react';
+import PropTypes from 'prop-types';
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const [ready, setReady] = useState(false);
+
   useEffect(() => {
-    if (!user) {
-      axios.get("/profile").then(({ data }) => {
+    const fetchUserProfile = async () => {
+      try {
+        const { data } = await axios.get('/profile');
         setUser(data);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      } finally {
         setReady(true);
-      });
+      }
+    };
+
+    if (!user) {
+      fetchUserProfile();
     }
-  }, []);
+  }, [user]);
+
   return (
     <UserContext.Provider value={{ user, setUser, ready }}>
       {children}
     </UserContext.Provider>
   );
 }
+
+UserContextProvider.propTypes = {
+  children: PropTypes.node.isRequired
+};
+
