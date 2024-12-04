38bd3e0ec7a754ce97afb440a479f77c9ccdddc3
n3bulaSt0rm <hainguyen.4work@gmail.com>
2024-12-04 13:26:24 +0700

    handle add, remove wishlist BE FE

--- back-end/src/services/wishlist.js
+++ back-end/src/services/wishlist.js
@@ -1,42 +1,77 @@
+const mongoose = require('mongoose');
 const wishlistRepository = require('../adapter/repositories/mongo/wishlist_repository');
 const placeRepository = require('../adapter/repositories/mongo/place_repository');
 const WishlistResponseDTO = require('../dtos/response/wishlist');
 const PlaceResponseDTO = require('../dtos/response/place');
 
 class WishlistService {
-    async createWishlist(wishlistData) {
-        const wishlist = await wishlistRepository.create(wishlistData);
-        return new WishlistResponseDTO(wishlist);
+    async addPlaceToWishlist(userId, placeId) {
+        if (!mongoose.Types.ObjectId.isValid(placeId)) {
+            throw new Error('invalid place ID');
+        }
+
+        const placeExists = await placeRepository.findById(placeId);
+        if (!placeExists) {
+            throw new Error('place does not exist');
+        }
+
+        const wishlist = await wishlistRepository.findByUserId(userId);
+
+        if (!wishlist) {
+            await wishlistRepository.create({
+                userId: userId,
+                placeIds: [placeId],
+            });
+        } else {
+            if (wishlist.placeIds.includes(placeId)) {
+                throw new Error('place already in wishlist');
+            }
+            const updatedWishlist = await wishlistRepository.update(
+                wishlist._id,
+                { $addToSet: { placeIds: placeId } }
+            );
+            if (!updatedWishlist) {
+                throw new Error('failed to add place to wishlist');
+            }
+        }
     }
 
-    async getWishlistByOwnerId(ownerId) {
-        const wishlist = await wishlistRepository.findByOwner(ownerId);
-        if (!wishlist) throw new Error('Wishlist not found');
+    async getWishlistByUserId(userId) {
+        const wishlist = await wishlistRepository.findByUserId(userId);
+        if (!wishlist || !wishlist.placeIds || wishlist.placeIds.length === 0) {
+            throw new Error('user has no favorite places');
+        }
 
-        const placeIds = wishlist.wishlist.map(item => item.place);
-
-        const places = await placeRepository.findByIds(placeIds);
-
-        const placesDTO = places.map(place => new PlaceResponseDTO(place));
+        const places = await placeRepository.findByIds(wishlist.placeIds);
+        const placesDTO = places.map((place) => new PlaceResponseDTO(place));
 
         return new WishlistResponseDTO(wishlist, placesDTO);
     }
 
-    async updateWishlist(id, updateData) {
-        const wishlist = await wishlistRepository.update(id, updateData);
-        if (!wishlist) throw new Error('Wishlist not found or update failed');
-        return new WishlistResponseDTO(wishlist);
-    }
+    async removePlaceFromWishlist(userId, placeId) {
+        if (!mongoose.Types.ObjectId.isValid(placeId)) {
+            throw new Error('invalid place ID');
+        }
 
-    async deleteWishlist(id) {
-        const wishlist = await wishlistRepository.delete(id);
-        if (!wishlist) throw new Error('Wishlist not found or delete failed');
-        return new WishlistResponseDTO(wishlist);
-    }
+        const wishlist = await wishlistRepository.findByUserId(userId);
+        if (!wishlist || wishlist.placeIds.length === 0) {
+            throw new Error('user has no favorite places');
+        }
 
-    async getAllWishlists() {
-        const wishlists = await wishlistRepository.findAll();
-        return wishlists.map(wishlist => new WishlistResponseDTO(wishlist));
+       const updatedPlaceIds = wishlist.placeIds.filter(
+            (id) => id.toString() !== placeId.toString()
+        );
+
+        if (updatedPlaceIds.length === wishlist.placeIds.length) {
+            throw new Error('place not found in wishlist');
+        }
+
+       const updatedWishlist = await wishlistRepository.update(wishlist._id, {
+            placeIds: updatedPlaceIds,
+        });
+        if (!updatedWishlist) {
+            throw new Error('failed to remove place from wishlist');
+        }
     }
 }
 
