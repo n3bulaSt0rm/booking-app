28ec57f631c87f64c3475f80d6b62cc8003816cf
hari <le31052003@gmail.com>
2024-12-05 20:19:37 +0700

    fix feedback feature

--- back-end/src/adapter/repositories/mongo/models/place.js
+++ back-end/src/adapter/repositories/mongo/models/place.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose');
+const mongoose= require('mongoose');
 const MongooseDelete = require('mongoose-delete');
 
 const Schema = mongoose.Schema;
-
 const PlaceSchema = new Schema(
     {
-        ownerId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-       },
-        title: { type: String, required: true },
+        owner: { 
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'User' 
+        },
+        title: String,
         address: String,
         photos: [String],
         description: String,
         perks: [String],
         extraInfo: String,
-        checkIn: { type: Number, min: 0, max: 24 },
-        checkOut: { type: Number, min: 0, max: 24 },
-        maxGuests: { type: Number, min: 1 },
-        price: { type: Number, min: 0 },
+        checkIn: Number,
+        checkOut: Number,
+        maxGuests: Number,
+        price: Number,
     },
     {
         timestamps: true,
-    }
+    },
 );
 
 PlaceSchema.plugin(MongooseDelete, { deletedAt: true, overrideMethods: 'all' });
