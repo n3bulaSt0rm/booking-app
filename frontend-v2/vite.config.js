1dace05e3856a3e97d9b60d86bebcc72bb9808a5
nguyenha-meiii <nguyenthuhat1k60@gmail.com>
2024-12-12 21:30:08 +0700

    fix deploy

--- frontend-v2/vite.config.js
+++ frontend-v2/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     host: true,
     port: 5173,
+  },
+  build: {
+    outDir: 'dist',
   }
-})
+});
